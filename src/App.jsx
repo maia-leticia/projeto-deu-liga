d2953f1098be1e798ae80e4b1c21a4d8ee49f6f1
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-06-30 17:55:51 -0300

    Update app
    redimensionei os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import ImgPrincipal from "../src/assets/img-principal.png"
 function App() {
   return (
     <>
-        <main className="relative bg-[#880808]">
+        <main className="relative bg-[#880808] h-[100vh]">
             <Header />
             <ElipseBackground className="absolute -translate-x-[20vw] -translate-y-[25vw]"/>
-            <div className="relative z-20 px-[70px]">
-              <h1 className="text-[50px] text-[white] font-sanchez" >INGREDIENTES</h1>
-            </div>
-            <div>
-              <img className="hidden md:flex" src={ImgPrincipal} alt="Food-image" />
-                 <ElipseBackground className="absolute translate-x-[15vw] -translate-y-[40vw]"/>
+            <div className="flex justify-between mt-[50px] ">
+              <div className="relative z-20 pl-[75px] ">
+                <h1 className="text-[70px] text-[white] font-sanchez" >INGREDIENTES</h1>
+              </div>
+              <div>
+                <img className="hidden lg:flex relative z-20 pt-[70px]" src={ImgPrincipal} alt="Food-image" />
+                   <ElipseBackground className="absolute translate-x-[20vw] -translate-y-[45vw]"/>
+              </div>
             </div>
         </main>
     </>
