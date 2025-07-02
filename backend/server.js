25645ac8e2d5961718d9ba8096f2e39ce5e2c368
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-07-02 18:52:25 -0300

    API chamada
    Criada requisição API

--- backend/server.js
+++ backend/server.js
@@ -7,31 +7,49 @@ app.use(cors());
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
-app.get("/token", async (req, res) => {
+async function getToken() {
   const body = new URLSearchParams();
   body.append("grant_type", "client_credentials");
   body.append("scope", "basic");
 
-  try {
-    const response = await fetch("https://oauth.fatsecret.com/connect/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization:
-          "Basic " +
-          Buffer.from(`${clientId}:${clientSecret}`).toString("base64"),
-      },
-      body: body.toString(),
-    });
+  const response = await fetch("https://oauth.fatsecret.com/connect/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        Buffer.from(`${clientId}:${clientSecret}`).toString("base64"),
+    },
+    body: body.toString(),
+  });
 
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error("Erro real:", error);
-    res.status(500).json({ error: error.message });
-  }
-});
+  const data = await response.json();
+  return data.access_token;
+}
 
+app.get("/autocomplete", async (req,res) =>{
+    try{
+        const query = req.query.query
+        if(!query){
+            return res.status(400).json({error: "Query é obrigatória"})
+        }
+        const token = await getToken()
+       const response = await fetch(
+        `https://platform.fatsecret.com/rest/server.api?method=foods.autocomplete&search_expression=${encodeURIComponent(query)}&format=json`,
+
+        {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        })
+        const data = await response.json();
+        res.json(data);
+
+    } catch (error){
+        console.error(error)
+        res.status(500).json({error: error.message})
+    }
+})
 
 app.listen(3001, () => {
   console.log("Backend rodando em http://localhost:3001");
--- src/api/searchfood.js
+++ src/api/searchfood.js
@@ -1,10 +1,10 @@
-export async function searchFood(search){
-    const suggestion = await fetch (`https://platform.fatsecret.com/rest/foods/search/v1?search_expression=${encodeURIComponent(search)}&format=json&page_number=0&max_results=5`)
-      headers: {
-        "Authorization": `Bearer SEU_TOKEN_AQUI`
-     }
-    if(!suggestion.ok) throw new Error("Erro ao buscar o alimento") 
-    const data = await response.json();
-    const suggestions = data.suggestions?.suggestion || [];
-    return suggestions.map(item => item.food_name);
+export async function searchFood(query){
+    try{
+      const response = await fetch(`http://localhost:3001/autocomplete?query=${encodeURIComponent(query)}`)
+      if (!response.ok) throw new Error("Erro na busca")
+        return await response.json()
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
 }
